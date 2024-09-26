3c5ccaaba76ded34af8a2311a5748b94e751dc70
isharafernando <isharafernando@gmail.com>
2024-09-26 17:57:53 -0400

    Replace Qt code with watchdog and implement file creation watcher

--- watch_data.py
+++ watch_data.py
@@ -1,21 +1,32 @@
-from PySide6.QtCore import QFileSystemWatcher
-from PySide6.QtCore import QCoreApplication
 import sys
+import logging
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
-def directory_changed(path):
-    print('Directory Changed: %s' % path)
+class Handler(FileSystemEventHandler):
+    def on_any_event(self, event):
+        # Process created files/directories
+        if event.event_type == 'created':
+            print(event.src_path)
 
-def file_changed(path):
-    print('File Changed: %s' % path)
+if __name__ == "__main__":
+    # set up logging object
+    logging.basicConfig(level=logging.INFO,
+                        format='%(message)s')
+    
+    # choose path to watch
+    path = sys.argv[1] if len(sys.argv) > 1 else '.'
 
-app = QCoreApplication(sys.argv)
+    # event handler
+    event_handler = Handler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
 
-paths = [
-    '../../Jay/run_data/run_005591'
-    ]
-
-fs_watcher = QFileSystemWatcher(paths)
-fs_watcher.directoryChanged.connect(directory_changed)
-fs_watcher.fileChanged.connect(file_changed)
-
-sys.exit(app.exec_())
+    # main loop
+    try:    
+        while observer.is_alive():
+            observer.join(1)
+    finally:    
+        observer.stop()
+        observer.join()
