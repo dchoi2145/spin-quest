ee5dd1281243e00560a21f2ee462243d4ea43dab
dchoi2145 <dchoi1579@gmail.com>
2024-12-01 22:25:03 -0500

    fixed some bugs

--- dashboard.py
+++ dashboard.py
@@ -23,11 +23,39 @@ detector_name_to_id_elements, max_elements = get_detector_info(SPECTROMETER_INFO
 detector_ids, element_ids = read_events(SPILL_PATH)
 initial_event_number = find_first_non_empty(detector_ids)
 
+# Define excluded detectors
+excluded_detectors = ["D1V", "D1Vp", "D1X", "D1Xp", "D1U", "D1Up"]
+excluded_detector_ids = [detector_name_to_id_elements[d][0] for d in excluded_detectors if d in detector_name_to_id_elements]
+
+# Remove excluded detectors from detector_name_to_id_elements
+filtered_detector_name_to_id_elements = {
+    key: value
+    for key, value in detector_name_to_id_elements.items()
+    if key not in excluded_detectors
+}
+
+# Filter out excluded detectors from the event data
+def filter_excluded(detector_ids, element_ids, excluded_ids):
+    filtered_detector_ids = []
+    filtered_element_ids = []
+
+    for det_id, elem_id in zip(detector_ids, element_ids):
+        if det_id not in excluded_ids:
+            filtered_detector_ids.append(det_id)
+            filtered_element_ids.append(elem_id)
+
+    return filtered_detector_ids, filtered_element_ids
+
+# Filter initial event data
+filtered_detector_ids, filtered_element_ids = filter_excluded(
+    detector_ids[initial_event_number], element_ids[initial_event_number], excluded_detector_ids
+)
+
 # Generate initial heatmap
-initial_heatmap = create_detector_heatmaps(
-    detector_ids[initial_event_number],
-    element_ids[initial_event_number],
-    detector_name_to_id_elements,
+main_heatmap = create_detector_heatmaps(
+    filtered_detector_ids,
+    filtered_element_ids,
+    filtered_detector_name_to_id_elements,
     max_elements,
 )
 
@@ -55,7 +83,7 @@ def navbar(current_path):
     )
 
 # Main View layout
-def main_view_layout(title, heatmap_fig, current_path):
+def main_view_layout(title, current_path):
     return html.Div([
         navbar(current_path),
         html.H1(title, className="text-center my-4"),
@@ -69,14 +97,14 @@ def main_view_layout(title, heatmap_fig, current_path):
             ]),
             dbc.Row([
                 dbc.Col([
-                    dcc.Graph(id="heatmap-graph", figure=heatmap_fig),
+                    dcc.Graph(id="heatmap-graph", figure=main_heatmap),
                 ]),
             ]),
         ]),
     ])
 
 # Checkbox View layout
-def checkbox_view_layout(title, heatmap_fig, current_path):
+def checkbox_view_layout(title, current_path):
     return html.Div([
         navbar(current_path),
         html.H1(title, className="text-center my-4"),
@@ -90,7 +118,7 @@ def checkbox_view_layout(title, heatmap_fig, current_path):
             ]),
             dbc.Row([
                 dbc.Col([
-                    dcc.Graph(id="heatmap-graph", figure=heatmap_fig),
+                    dcc.Graph(id="heatmap-graph", figure=main_heatmap),
                 ]),
             ]),
             dbc.Row([
@@ -117,32 +145,40 @@ app.layout = html.Div([
 @app.callback(Output("page-content", "children"), [Input("url", "pathname")])
 def render_page(pathname):
     if pathname == "/checkbox":
-        return checkbox_view_layout("Checkbox View", initial_heatmap, pathname)
+        return checkbox_view_layout("Checkbox View", pathname)
     else:
-        return main_view_layout("Main View", initial_heatmap, pathname)
+        return main_view_layout("Main View", pathname)
 
-# Callback to update the heatmap based on selected groups
+# Callback to update the heatmap dynamically
 @app.callback(
     Output("heatmap-graph", "figure"),
     [Input("update-button", "n_clicks"),
-     Input("grouped-detector-checklist", "value")],
-    State("event-number-input", "value"),
+     Input("grouped-detector-checklist", "value"),
+     State("event-number-input", "value")],
 )
 def update_heatmap(n_clicks, selected_groups, event_number):
     # Get detectors to display based on selected groups
-    selected_detectors = [
-        detector for group in selected_groups for detector in group_to_detectors[group]
-    ]
+    if selected_groups:
+        selected_detectors = [
+            detector for group in selected_groups for detector in group_to_detectors[group]
+        ]
+    else:
+        selected_detectors = []
 
     # Update detector visibility
-    for detector in detector_name_to_id_elements:
-        detector_name_to_id_elements[detector][-1] = detector in selected_detectors
+    for detector in filtered_detector_name_to_id_elements:
+        filtered_detector_name_to_id_elements[detector][-1] = detector in selected_detectors
+
+    # Filter data for the updated event number
+    filtered_ids, filtered_elements = filter_excluded(
+        detector_ids[event_number], element_ids[event_number], excluded_detector_ids
+    )
 
     # Generate new heatmap
     return create_detector_heatmaps(
-        detector_ids[event_number],
-        element_ids[event_number],
-        detector_name_to_id_elements,
+        filtered_ids,
+        filtered_elements,
+        filtered_detector_name_to_id_elements,
         max_elements,
     )
 
